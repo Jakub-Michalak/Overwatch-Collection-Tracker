6f8caf303bb7e3c1dd37cd80b7eae489e53c1f1e
Jakub Michalak <jakubmichalak@Jakubs-MacBook-Pro.local>
2018-08-05 22:00:47 +0200

    v0.1

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+	<meta charset="utf-8"/>
+	<title>Overwatch Collection Tracker</title>
+	<link rel="stylesheet" type="text/css" href="theme.css">
+</head>
+<body>
+
+	<div id="container">
+	<div id="middle">
+		<div id="home_buttons">
+		<div class="home_button" id="heroes">
+			<div class="title">Heroes</div>
+		</div>
+		<div class="home_button">
+			<div class="title">Events</div>
+		</div>
+		<div class="home_button" id="spray">
+			<div class="title">Icons and sprays</div>
+		</div>
+		<div class="home_button" id="stats">
+			<div class="title">Stats</div>
+		</div>
+		</div>
+	</div>
+	</div>
+	<!-- <div>Icons made by <a href="https://www.flaticon.com/authors/madebyoliver" title="Madebyoliver">Madebyoliver</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div> -->
+<div id="gradient">
+</div>
+</body>
